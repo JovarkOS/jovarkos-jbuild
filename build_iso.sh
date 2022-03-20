a1d54d2751bf2e9abecf51d75bef769a2bd9cf32
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2022-03-20 14:27:35 -0400

    Added ability to copy profiles

--- build_iso.sh
+++ build_iso.sh
@@ -3,31 +3,54 @@
 WORKING="working"
 OUTPUT="output"
 SOURCE="archlive"
-# ISO="$OUTPUT/jovarkos-$(date +%Y.%m.%d).iso"
+ISO="jovarkos-$(date +%Y.%m.%d)-x86_64.iso"
+
+function generic_error() {
+    echo "Incorrect syntax or options. Please use the '--help' flag for more information."
+    exit 1
+}
+
 
 if [ "$1" == "build" ] || [ "$1" == "-b" ]; then
     /usr/bin/mkarchiso -v -w $WORKING -o $OUTPUT $SOURCE
 fi
 
-if [ "$1" == "" ]; then
-    echo "Incorrect syntax or options. Please use the '--help' flag for more information."
+if [ "$1" == "" ] || [ "$(test -z $3)" == 0 ]; then
+    generic_error
 fi
 
 if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-    __usage="Usage: Script/build_iso.sh [FLAGS]
+    __usage="Usage: ./build_iso.sh [FLAGS] [OPTIONS]
 
     Options:
-    -b, --build                  Build fresh ISO image using defaults from the archlive/profiledef.sh file
+    -b, --build                  Build fresh ISO image using defaults from the $SOURCE/profiledef.sh file
     -r, --run                    Build and run fresh ISO image
+    -p, --profile <profile name>   Clean working directory and create 
     -h, --help                   Print this help message
-    -v, --version                Get the version of this script"
+    -v, --version                Get the version of this script
+    
+    Options for '-p' and '--profile':
+    Profile - see <https://wiki.archlinux.org/title/Archiso#Prepare_a_custom_profile>
+    for more information
+
+         - Use either 'releng' or 'baseline' as the profile name. (Used in Official Monthly Builds)
+         - Profiles live in the /usr/share/archiso/configs/ directory. (Minimal live configuration)
+    
+    "
+
+
     echo "$__usage"
 fi
 
-if [ "$1" == "-v" ]; then
-    echo "JovarkOS ISO build script v2021.08.29.r2"
+if [ "$1" == "-p" ] || [ "$1" == "--profile" ]; then
+    cp -r "/usr/share/archiso/configs/$2/" $SOURCE
+    echo "Copied profile $2 to $SOURCE"
 fi
 
-if [ "$1" == "-r" ]; then
-    /usr/bin/run_archiso -i $OUTPUT/August/jovarkos-2021.08.26-x86_64.iso
+if [ "$1" == "-v" ] || [ "$1" == "--version" ]; then
+    echo "JovarkOS ISO build script v2022.03.20.r1"
+fi
+
+if [ "$1" == "-r" ] || [ "$1" == "--run" ]; then
+    /usr/bin/run_archiso -i $OUTPUT/$ISO
 fi
